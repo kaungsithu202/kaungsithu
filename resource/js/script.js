8be949d7fccf20b9a464d01485a0eb5ee7bddd3a
kaungsithu202 <oldmanwhitebest@gmail.com>
2021-10-17 20:04:32 +0630

    jquery

---
+++ resource/js/script.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+    // sticky Nav
+    $('.js--section-feature').waypoint(function(direction){
+        if(direction == 'down'){
+            $('nav').addClass('sticky');
+
+        }else{
+        $('nav').removeClass('sticky');
+        }
+    },{
+        offset:'150px'
+    });
+    // scroll button effect
+   
+   $('.js-btn-full').click(function(){
+       $('html,body').animate({scrollTop :$('.js--section-plan').offset().top},1000);
+       return false;
+   })
+   $('.js-btn-ghost').click(function(){
+       $('html,body').animate({scrollTop : $('.js--section-feature').offset().top},1000);
+       return false;
+   })
+
+//    Scroll Nav button effect
+   $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+});
+ 
